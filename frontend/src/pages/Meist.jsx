151b6d08812b607656a5af3e596995b4d4c869aa
MathiasKaasma <106476093+MathiasKaasma@users.noreply.github.com>
2024-08-20 18:19:17 +0300

    finish frontend example first version

--- frontend/src/pages/Meist.jsx
+++ frontend/src/pages/Meist.jsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import ContactCard from "../components/ContactCard";
 
 function Meist() {
+  const [contacts, setContacts] = useState([
+    { name: "Mathias", age: 21 },
+    { name: "Martin", age: 20 },
+  ]);
+
   return (
     <div>
       <h2>Meist</h2>
       <p>Tekst teie endi kohta!</p>
-      <ContactCard name={"Mathias"} age={21} />
-      <ContactCard name={"Martin"} age={20} />
+      {contacts.map((contact) => (
+        <ContactCard name={contact.name} age={contact.age} />
+      ))}
     </div>
   );
 }
