096110e6e898809a3fbafd055547fc402955e377
MathiasKaasma <106476093+MathiasKaasma@users.noreply.github.com>
2024-09-27 16:53:41 +0300

    init backend

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import { Pool } from "pg";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
